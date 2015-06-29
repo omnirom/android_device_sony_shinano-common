0e446fa71ccc9c3d1ee8b191c02d648940647729
Jake Whatley <jacob.whatley91@gmail.com>
2015-06-29 15:35:06 -0400

    shinano-common: healthd: Add tricolor led support
    PS2: added sysfs entries for LED colors from Humberto Borba
    PS3: use correct sysfs paths
    PS4: move the OmniROM changes to BoardConfigOmni
    
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: I7c8f8bd06c2d8ccfebaceab072423017ddca8ceb

--- BoardConfigOmni.mk
+++ BoardConfigOmni.mk
@@ -23,6 +23,10 @@ SONY_AOSP ?= true
 
 # Charger
 HEALTHD_FORCE_BACKLIGHT_CONTROL := true
+HEALTHD_ENABLE_TRICOLOR_LED := true
+RED_LED_PATH := /sys/devices/01-qcom,leds-d000/leds/led:rgb_red/brightness
+GREEN_LED_PATH := /sys/devices/01-qcom,leds-d000/leds/led:rgb_green/brightness
+BLUE_LED_PATH := /sys/devices/01-qcom,leds-d000/leds/led:rgb_blue/brightness
 
 # TWRP Recovery
 TARGET_NO_SEPARATE_RECOVERY := true
