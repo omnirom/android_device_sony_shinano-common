7416d81ae6a463fdc8ff875b970be8bcb304e07b
Alexander Diewald <Diewi@diewald-net.com>
2017-05-09 12:16:18 +0200

    sony: shinano: Correct makefile structure.
    Signed-off-by: Alexander Diewald <Diewi@diewald-net.com>

--- BoardConfigCommonOmni.mk
+++ BoardConfigCommonOmni.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-include device/sony/shinano-common/BoardCommonConfigOmni.mk
+include device/sony/shinano-common/BoardConfigCommon.mk
 
 #Inherit TWRP Config
 include device/sony/shinano-common/twrp.mk
