9978d44ddc15666f428c869d802843732884d7bd
Alexander Diewald <Diewi@diewald-net.com>
2017-05-28 20:56:11 +0200

    shinano: Remove bootline (-->common) & set to permissive.
    Change-Id: I628cfcf2f7640788106b5ce6eb16fcc23c6cc3ac
    Signed-off-by: Alexander Diewald <Diewi@diewald-net.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -32,9 +32,6 @@ BOARD_KERNEL_IMAGE_NAME := zImage-dtb
 # Kernel information
 BOARD_KERNEL_BASE     := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_CMDLINE  := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 dwc3.maximum_speed=high dwc3_msm.prop_chg_detect=Y
-BOARD_KERNEL_CMDLINE  += androidboot.selinux=permissive
-#BOARD_KERNEL_CMDLINE  += androidboot.selinux=disabled
 BOARD_MKBOOTIMG_ARGS  := --ramdisk_offset 0x02000000 --tags_offset 0x01E00000
 
 KERNEL_TOOLCHAIN        := $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin
