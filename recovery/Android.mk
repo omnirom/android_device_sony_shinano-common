cc6b80254149edd99f92e2ee1a706b868cf7635a
nailyk-fr <nailyk_git@nailyk.fr>
2018-03-29 14:04:14 +0200

    [DNM] TWRP: dirty unzip workaround
    Change-Id: Id9e30e60a7fa66426c2b7d5e971f4436c03be82b
    Signed-off-by: nailyk-fr <nailyk_git@nailyk.fr>

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -8,7 +8,8 @@ LOCAL_SRC_FILES         := root/etc/twrp.fstab
 LOCAL_MODULE_PATH       := $(TARGET_RECOVERY_ROOT_OUT)/etc
 
 LOCAL_POST_INSTALL_CMD += \
-    cp -uv $(TARGET_ROOT_OUT)/init.real $(TARGET_RECOVERY_ROOT_OUT)/init ;
+    cp -uv $(TARGET_ROOT_OUT)/init.real $(TARGET_RECOVERY_ROOT_OUT)/init ; \
+    cp -uv $(LOCAL_PATH)/root/sbin/unzip $(TARGET_RECOVERY_ROOT_OUT)/sbin/unzip ;
 
 LOCAL_ADDITIONAL_DEPENDENCIES += \
     twrpdec \
