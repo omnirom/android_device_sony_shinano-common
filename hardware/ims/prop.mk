a0a0350ac9d4da11115a2404a99f5414cf26d5cc
Alexander Diewald <diewald@fortiss.org>
2018-06-15 16:12:35 +0200

    Shinano: IMS: Add required properties.
    Change-Id: I9a95b2e152d665020468aad582519e564ebdf5cf
    Signed-off-by: Alexander Diewald <diewald@fortiss.org>

---
+++ hardware/ims/prop.mk
@@ -0,0 +1,8 @@
+PRODUCT_PROPERTY_OVERRIDES += \
+	persist.radio.calls.on.ims=1 \
+	persist.ims.volte=true \
+	persist.dbg.volte_avail_ovr=1 \
+	persist.radio.RATE_ADAPT_ENABLE=1 \
+	persist.radio.ROTATION_ENABLE=1 \
+	persist.radio.VT_ENABLE=1 \
+	persist.radio.VT_HYBRID_ENABLE=1
